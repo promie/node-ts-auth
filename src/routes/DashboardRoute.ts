4fe488aea2efb364379fb0193505320d1e87f509
Promie Yutasane <promie.yutasane@gmail.com>
2023-10-13 03:50:30 +1100

    NTA-12 applied the verify token middleware to the dashboard route

--- src/routes/DashboardRoute.ts
+++ src/routes/DashboardRoute.ts
@@ -1,8 +1,13 @@
 import express from 'express'
 import { DashboardController } from '../controllers'
+import { AuthMiddleware } from '../middlewares'
 
 const router = express.Router()
 
-router.get('/user', DashboardController.getUserDetails)
+router.get(
+  '/user',
+  AuthMiddleware.verifyToken,
+  DashboardController.getUserDetails,
+)
 
 export default router
