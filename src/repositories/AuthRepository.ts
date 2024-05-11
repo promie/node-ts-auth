41e3209630141836deaaf07bdadab3bf31127c78
Promie Yutasane <promie.yutasane@gmail.com>
2024-05-12 08:29:58 +1000

    NTA-32 save user to the database upon registration

--- src/repositories/AuthRepository.ts
+++ src/repositories/AuthRepository.ts
@@ -1,7 +1,15 @@
 import { IUser } from '../types/user'
+import prismadb from '../utils/prisma'
 
 const register = async (user: IUser) => {
   // save to user to the database
+  await prismadb.user.create({
+    data: {
+      username: user.username,
+      password: user.password,
+    },
+  })
+
   return user
 }
 
